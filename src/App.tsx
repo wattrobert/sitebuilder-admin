8b191805a6e37ce7044f9bbd25416df0f444f61d
Robert Watt <hundo.watt@gmail.com>
2018-12-21 14:31:54 -0600

    Basic layout & links

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
+import Badge from "@material-ui/core/Badge";
+import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
+import InputBase from "@material-ui/core/InputBase";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import MailIcon from "@material-ui/icons/Mail";
 import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import ServerIcon from "@material-ui/icons/DnsOutlined";
+import ProductIcon from "@material-ui/icons/LocalOfferOutlined";
+import SitesIcon from "@material-ui/icons/BookmarkBorderOutlined";
+import ComponentsIcon from "@material-ui/icons/ExtensionOutlined";
+import TemplatesIcon from "@material-ui/icons/ViewCompactOutlined";
+import AccountIcon from "@material-ui/icons/PersonOutlineRounded";
+import NotificationsIcon from "@material-ui/icons/NotificationsOutlined";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { createStyles, withStyles, Theme } from "@material-ui/core/styles";
@@ -30,10 +39,54 @@ const styles = (theme: Theme) =>
       }
     },
     appBar: {
+      boxShadow: "none",
+      borderBottom: "1px solid " + theme.palette.grey[300],
       marginLeft: drawerWidth,
       [theme.breakpoints.up("sm")]: {
         width: `calc(100% - ${drawerWidth}px)`
       }
+    },
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: theme.palette.grey[100],
+      "&:hover": {
+        backgroundColor: theme.palette.grey[200]
+      },
+      marginRight: theme.spacing.unit * 2,
+      marginLeft: theme.spacing.unit * 2,
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: 0,
+        width: "auto"
+      }
+    },
+    searchIcon: {
+      width: theme.spacing.unit * 6,
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    inputRoot: {
+      color: "inherit",
+      width: "100%"
+    },
+    inputInput: {
+      paddingTop: theme.spacing.unit,
+      paddingRight: theme.spacing.unit,
+      paddingBottom: theme.spacing.unit,
+      paddingLeft: theme.spacing.unit * 6,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: 200
+      }
+    },
+    grow: {
+      flexGrow: 1
     },
     menuButton: {
       marginRight: 20,
@@ -73,37 +126,52 @@ class App extends React.Component<AppProps, AppState> {
     const { classes, theme } = this.props;
 
     const drawer = (
-      <div>
-        <div className={classes.toolbar} />
+      <React.Fragment>
+        <Toolbar>
+          <Typography variant="headline" color="inherit" noWrap>
+            Sitebuilder
+          </Typography>
+        </Toolbar>
         <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <ListItem button>
+            <ListItemIcon>
+              <ServerIcon />
+            </ListItemIcon>
+            <ListItemText primary="Servers" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <ProductIcon />
+            </ListItemIcon>
+            <ListItemText primary="Products" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <SitesIcon />
+            </ListItemIcon>
+            <ListItemText primary="Sites" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <TemplatesIcon />
+            </ListItemIcon>
+            <ListItemText primary="Templates" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <ComponentsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Components" />
+          </ListItem>
         </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
+      </React.Fragment>
     );
 
     return (
       <div className={classes.root}>
         <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
           <Toolbar>
             <IconButton
               color="inherit"
@@ -113,14 +181,38 @@ class App extends React.Component<AppProps, AppState> {
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Responsive drawer
-            </Typography>
+            <Hidden smUp implementation="css">
+              <Typography variant="h6" color="inherit" noWrap>
+                SB
+              </Typography>
+            </Hidden>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
+            </div>
+            <div className={classes.grow} />
+            <Button color="inherit">Support</Button>
+            <IconButton color="inherit" aria-label="Notifications">
+              <Badge badgeContent={4} color="primary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton color="inherit" aria-label="Account">
+              <AccountIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
         <nav className={classes.drawer}>
           {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
+          <Hidden smUp implementation="js">
             <Drawer
               variant="temporary"
               anchor={theme.direction === "rtl" ? "right" : "left"}
@@ -133,7 +225,7 @@ class App extends React.Component<AppProps, AppState> {
               {drawer}
             </Drawer>
           </Hidden>
-          <Hidden xsDown implementation="css">
+          <Hidden xsDown implementation="js">
             <Drawer
               classes={{
                 paper: classes.drawerPaper
