b292b4676726e7366c5206c5abbd84e9c7e28db7
Robert Watt <hundo.watt@gmail.com>
2018-12-21 13:42:28 -0600

    Add material-ui/core & material-ui/icons

---
+++ src/constants/theme.tsx
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1B4079"
+    },
+    secondary: {
+      main: "#B9DCE1"
+    }
+  }
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
+import appTheme from "./constants/theme";
+
+ReactDOM.render(
+  <MuiThemeProvider theme={appTheme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
